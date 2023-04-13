69d670dafe58d996ceafa267fa3ac02931aae95e
ojimenezl <ojimenezl@est.ups.edu.ec>
2023-04-13 14:36:53 -0500

    heroku

--- src/enviroment/enviroment.dev.ts
+++ src/enviroment/enviroment.dev.ts
@@ -5,4 +5,4 @@ export const PUERTO = 3000;
 const username = 'tuweka';
 const contra = 'mZ0WAE0AuoJlyBWb';
 export const MONGODB_DATABASE = 'tuweka';
-export const MONGODB_CONEXION = `mongodb+srv://${username}:${contra}@cluster0.fh2ez.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority`
+export const MONGODB_CONEXION = `mongodb+srv://adelciaec:2468@cluster0.e1ndwgr.mongodb.net/?retryWrites=true&w=majority`
